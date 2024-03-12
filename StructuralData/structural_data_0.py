444265841370cc7df094fefb7a1298ba0038eaaa
Baran Erdem <109768322+erdembaran@users.noreply.github.com>
2024-03-12 09:35:27 +0300

    update

--- StructuralData/structural_data_0.py
+++ StructuralData/structural_data_0.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = {
     "name": ["cristiano", "lionel", "dusan", "edin", "karim"],
     "lastname": ["ronaldo", "messi", "tadic", "dzeko", "benzema"],
-    "team": ["al-nassr", "miami", "fenerbahce", "fenerbahce", "al-ittihad"],
+    "team": ["al-nassr", "miami", "fenerbahce", "fenerbahce", "al-ittihadx"],
 }
 
 df = pd.DataFrame(data)
