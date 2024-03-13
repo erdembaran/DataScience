1ee462b70d9d9422b6e9e842375253b2d0c925c9
Baran Erdem <109768322+erdembaran@users.noreply.github.com>
2024-03-13 14:29:54 +0300

    feat: create regression score function module

---
+++ LinearRegression/linear_regression_8.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+data = pd.read_csv('../csv/sales.csv')
+months = data[['Months']]
+sales = data[['Sales']]
+
+x_train, x_test,y_train,y_test = train_test_split(months,sales,test_size=0.33, random_state=0)
+    
+lr = LinearRegression()
+
+lr.fit(x_train, y_train)
+
+prediction = lr.predict([[12.0]]) # specific month.
+
+print("value of r2 score: ", r2_score(y_test, lr.predict(x_test)))
